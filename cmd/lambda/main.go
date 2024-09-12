ffe14147108f7ef6e500a759ebf4deae8a98db48
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2024-09-12 12:29:55 +0100

    add example of using CF SDK inside webhook handler (#1)

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
 
+	"connectrpc.com/connect"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/common-fate/example-webhook-handler/pkg/audit"
+	"github.com/common-fate/sdk/config"
+	accessv1alpha1 "github.com/common-fate/sdk/gen/commonfate/access/v1alpha1"
+	"github.com/common-fate/sdk/service/access/grants"
 )
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (*string, error) {
@@ -19,7 +24,20 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return nil, fmt.Errorf("invalid authorization header")
 	}
 
-	err := json.Unmarshal([]byte(request.Body), &logEvent)
+	cfg, err := config.New(ctx, config.Opts{
+		APIURL:       os.Getenv("CF_API_URL"),
+		ClientID:     os.Getenv("CF_OIDC_CLIENT_ID"),
+		AccessURL:    os.Getenv("CF_API_URL"),
+		ClientSecret: os.Getenv("CF_OIDC_CLIENT_SECRET"),
+		OIDCIssuer:   os.Getenv("CF_OIDC_ISSUER"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	client := grants.NewFromConfig(cfg)
+
+	err = json.Unmarshal([]byte(request.Body), &logEvent)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse audit log event: %v", err)
 	}
@@ -35,17 +53,49 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	)
 
 	var matchesPermissionSet bool
+	var grantID string
+	var accessRequestID string
 
 	for _, target := range logEvent.Targets {
 		if target.Type == "AWS::IDC::PermissionSet" && target.ID == "<replace this with the ID of the Permission Set to alert on>" {
 			matchesPermissionSet = true
 		}
+
+		if target.Type == "Access::Grant" {
+			grantID = target.ID
+		}
+
+		if target.Type == "Access::Request" {
+			accessRequestID = target.ID
+		}
 	}
 
-	if logEvent.Action == audit.ActionGrantActivated && matchesPermissionSet {
-		logger.Info("breakglass access was activated")
+	if logEvent.Action == audit.ActionGrantRequested && matchesPermissionSet {
+		if grantID == "" {
+			return nil, errors.New("could not extract grant ID")
+		}
+		if accessRequestID == "" {
+			return nil, errors.New("could not extract Access Request ID")
+		}
 
-		// you can add additional custom logic here, such as calling a PagerDuty webhook.
+		grantResponse, err := client.GetGrant(ctx, connect.NewRequest(&accessv1alpha1.GetGrantRequest{
+			Id: grantID,
+		}))
+		if err != nil {
+			return nil, err
+		}
+
+		if grantResponse.Msg.Grant.Status == accessv1alpha1.GrantStatus_GRANT_STATUS_PENDING {
+			accessRequestURL := fmt.Sprintf("https://commonfate.example.com/access/requests/%s", accessRequestID)
+
+			logger.Info("access request is pending and requires a manual review",
+				slog.String("grant_id", grantID),
+				slog.String("access_request_id", accessRequestID),
+				slog.String("access_request_url", accessRequestURL),
+			)
+			// you can add additional custom logic here, such as calling a PagerDuty webhook.
+		}
+
 	}
 
 	return nil, nil
--- go.mod
+++ go.mod
@@ -3,12 +3,44 @@ module github.com/common-fate/example-webhook-handler
 go 1.22.5
 
 require (
+	connectrpc.com/connect v1.14.0
 	github.com/aws/aws-lambda-go v1.47.0
 	github.com/common-fate/sdk v1.57.0
 )
 
 require (
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/99designs/go-keychain v0.0.0-20191008050251-8e49817e8af4 // indirect
+	github.com/99designs/keyring v1.2.2 // indirect
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/common-fate/clio v1.2.3 // indirect
+	github.com/danieljoos/wincred v1.1.2 // indirect
+	github.com/dvsekhvalnov/jose2go v1.6.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.2 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gsterjov/go-libsecret v0.0.0-20161001094733-a6f4afe4910c // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mtibben/percent v0.2.1 // indirect
+	github.com/muhlemmer/gu v0.3.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/zitadel/logging v0.6.0 // indirect
+	github.com/zitadel/oidc/v3 v3.26.0 // indirect
+	github.com/zitadel/schema v1.3.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
